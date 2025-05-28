be63736882a874270da3f5b012b7c81f5e9ceef7
Mehedi-9T9 <mehedi.hasan9t9@gmail.com>
2025-05-28 08:32:58 +0600

    unhandledRejection uncaughtException error handle

--- src/middleware/globalErrorHandler.ts
+++ src/middleware/globalErrorHandler.ts
@@ -5,6 +5,7 @@ import config from "../config"
 import simplifiedError from "../app/errors/zodErrorHandle"
 import validationError from "../app/errors/validationErrorHandle"
 import castErrorHandle from "../app/errors/castErrorHandle"
+import AppError from "../app/errors/appError"
 
 const globalErrorHandler =(error:any,req:Request,res:Response,next:NextFunction)=>{
   let statusCode = error.statusCode || 500
@@ -24,15 +25,37 @@ message =simplified.message
 }else if(error.name ==="ValidationError"){
   const simplified =validationError(error)
   statusCode =simplified.statusCode;
-errorMessage =simplified.errorMessage;
-path =simplified.path;
-message =simplified.message
+  errorMessage =simplified.errorMessage;
+  path =simplified.path;
+  message =simplified.message
 }else if(error.name ==="CastError"){
    const simplified =castErrorHandle(error)
-  statusCode =simplified.statusCode;
-errorMessage =simplified.errorMessage;
-path =simplified.path;
-message =simplified.message
+   statusCode =simplified.statusCode;
+   errorMessage =simplified.errorMessage;
+   path =simplified.path;
+   message =simplified.message
+} else if(error?.code ===11000){
+  //  const simplified =castErrorHandle(error)
+   statusCode =400;
+   errorMessage ="Dublicate Error";
+  ;
+   message =error?.message
+}
+ else if(error instanceof AppError){
+  //  const simplified =castErrorHandle(error)
+  console.log('ami app error');
+   statusCode =error?.statusCode;
+   errorMessage =error?.message;
+  ;
+   message =error?.message
+}
+ else if(error instanceof Error){
+  //  const simplified =castErrorHandle(error)
+  console.log('ami app error');
+   statusCode =400;
+   errorMessage =error?.message;
+  ;
+   message =error?.message
 }
 
 //Error Pattern
--- src/server.ts
+++ src/server.ts
@@ -3,11 +3,12 @@ import app from "./app";
 import mongoose from "mongoose";
 import config from "../src/config";
 
-// const port = 3000;
 
+// const port = 3000;
+let server:any;
 (async()=> {
     try {
-        app.listen(config.PORT, () => {
+        server = app.listen(config.PORT, () => {
             console.log(config.PORT);
             console.log(`Example app listening on port ${config.PORT}`);
         });
@@ -18,3 +19,20 @@ import config from "../src/config";
     }
     
 })();
+process.on('unhandledRejection', ()=>{
+    console.log('unhandledRejection error, 😡 shutting down server');
+    if(server){
+        server.close(()=>{
+            process.exit(1)
+        })
+    }
+    process.exit(1)
+})
+
+process.on('uncaughtException',()=>{
+    console.log('uncaughtException error, 😡 shutting down server');
+    process.exit(1)
+})
+
+// console.log(x);
+// Promise.reject()
